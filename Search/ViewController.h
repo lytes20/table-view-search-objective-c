686bdbe05b44efff39cbdc9d272197694d085956
Bamuleseyo Gideon <gideonbamuleseyo@gmail.com>
2017-10-30 14:34:14 +0300

    Initial Commit

---
+++ Search/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Search
+//
+//  Created by Gideon Bamuleseyo on 10/30/17.
+//  Copyright © 2017 lytestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Search/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Search
+//
+//  Created by Gideon Bamuleseyo on 10/30/17.
+//  Copyright © 2017 lytestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
